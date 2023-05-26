41a9375b4f98db55908e1da67f88ea52e40cf117
afrarods <93016990+afrarods@users.noreply.github.com>
2023-05-26 17:18:47 +0100

    2 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -8,27 +8,22 @@ export interface Product {
 export const products = [
   {
     id: 1,
-    name: 'Phone XL',
-    price: 799,
-    description: 'A large phone with one of the best screens'
+    name: 'iPhone 14 Pro Max',
+    price: 1221,
+    description: 'Processor A16 Bionic, RAM 6GB, Internal Storage 128GB, '
   },
   {
     id: 2,
-    name: 'Phone Mini',
-    price: 699,
-    description: 'A great phone with one of the best cameras'
+    name: 'Samsung Galaxy S22 Ultra 5G',
+    price: 896,
+    description: 'Processor Exynos 2200, RAM 6GB, Internal Storage 128GB'
   },
   {
     id: 3,
-    name: 'Phone Standard',
-    price: 299,
-    description: ''
+    name: 'HUAWEI P40 Lite',
+    price: 204,
+    //description: 'Processor Kirin 810, RAM 8GB, Internal Storage 64GB'
   }
 ];
 
 
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
